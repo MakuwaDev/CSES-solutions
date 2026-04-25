3352e27dc5fd12a7e8732fd9ae86a334a8089aac
Krzysztof Hałubek <github@rob.pl>
2026-04-25 13:42:24 +0200

    binomial coefficients

---
+++ 06 - Mathematics/binomial_coefficients.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <array>
+
+constexpr std::size_t MAX = 1e6 + 6;
+constexpr std::size_t MOD = 1e9 + 7;
+
+std::array<std::size_t, MAX> fact_mod;
+
+std::size_t pow_mod(std::size_t x, std::size_t exp) {
+    std::size_t res = 1, pow = x;
+
+    while (exp > 0) {
+        if (exp & 1) {
+            res = (res * pow) % MOD;
+        }
+
+        pow = (pow * pow) % MOD;
+        exp >>= 1;
+    }
+
+    return res;
+}
+
+std::size_t newton_mod(std::size_t a, std::size_t b) {
+    return (fact_mod[a] * pow_mod((fact_mod[b] * fact_mod[a - b]) % MOD, MOD - 2)) % MOD;
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    fact_mod[1] = 1;
+    for (std::size_t i = 2; i < MAX; ++i) {
+        fact_mod[i] = (fact_mod[i - 1] * i) % MOD;
+    }
+
+    std::size_t n, a, b;
+    std::cin >> n;
+
+    while (n--) {
+        std::cin >> a >> b;
+
+        if (!b) {
+            std::cout << "1\n";
+        } else {
+            std::cout << newton_mod(a, b) << "\n";
+        }
+    }
+
+    return 0;
+}
