c30f693b1346749b99ac62b07a4a3c536636ca3b
Krzysztof Hałubek <github@rob.pl>
2026-05-22 10:25:45 +0200

    counting paths

---
+++ 05 - Tree Algorithms/counting_paths.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+
+constexpr std::size_t MAXN = 2e5 + 5;
+constexpr std::size_t MAXPOW = 20;
+
+std::vector<std::size_t> graph[MAXN];
+std::size_t depth[MAXN];
+std::size_t ancestors[MAXN][MAXPOW + 1];
+std::size_t dp[MAXN];
+
+void init(std::size_t v = 1, std::size_t father = 1) {
+	depth[v] = depth[father] + 1;
+	ancestors[v][0] = father;
+
+	for (std::size_t i = 1; i <= MAXPOW; ++i) {
+		ancestors[v][i] = ancestors[ancestors[v][i - 1]][i - 1];
+    }
+
+	for (auto x : graph[v]) {
+		if (x != father) init(x, v);
+    }
+}
+
+std::size_t lca(std::size_t a, std::size_t b) {
+	if (a == b) {
+        return a;
+    }
+
+	if (depth[a] > depth[b]) {
+        std::swap(a, b);
+    }
+
+	for (std::size_t i = MAXPOW; i <= MAXPOW; --i) {
+		if (depth[ancestors[b][i]] >= depth[a]){
+            b = ancestors[b][i];
+        }
+    }
+
+	if (a == b) {
+        return a;
+    }
+
+	for (std::size_t i = MAXPOW; i <= MAXPOW; --i) {
+		if (ancestors[a][i] != ancestors[b][i]) {
+			a = ancestors[a][i];
+			b = ancestors[b][i];
+		}
+    }
+
+	return ancestors[a][0];
+}
+
+std::size_t dfs(std::size_t v = 1, std::size_t p = 1) {
+    for (auto const& u : graph[v]) {
+        if (u != p) {
+            dp[v] += dfs(u, v);
+        }
+    }
+
+    return dp[v];
+}
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	std::size_t n, q, a, b, l;
+
+	std::cin >> n >> q;
+
+	for (std::size_t i = 0; i < n - 1; ++i) {
+		std::cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+
+	depth[1] = -1;
+	init();
+
+    while (q--) {
+        std::cin >> a >> b;
+        l = lca(a, b);
+        
+        ++dp[a];
+        ++dp[b];
+        --dp[l];
+
+        if (l != 1) {
+            --dp[ancestors[l][0]];
+        }
+    }
+
+    dfs();
+
+    for (std::size_t i = 1; i <= n; ++i) {
+        std::cout << dp[i] << " ";
+    }
+
+    std::cout << "\n";
+    return 0;
+}
